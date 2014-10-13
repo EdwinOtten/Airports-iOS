ddf0651ad79b039ae8167d76897019407b7c461c
Edwin <edwin@Administrators-iMac-3.local>
2014-10-13 13:45:48 +0200

    Initial Commit

---
+++ Airports-iOS/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  Airports-iOS
+//
+//  Created by Edwin on 13/10/14.
+//  Copyright (c) 2014 Edwin Otten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
